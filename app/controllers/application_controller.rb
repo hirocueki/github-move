909c39af28f881304447efdec34c0997df9c6988
Hiroshi Ueki <hirocueki@gmail.com>
2018-02-23 02:06:30 +0000

    Update root render 'Hola'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
+    render html: 'Hello, world!'
+  end
+  
+  def hola
     render html: '¡Hola, mundo!'
   end
   
